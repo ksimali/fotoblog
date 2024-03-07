e1440896b5720f3bdde21a759b211b7a0d3f5c56
ksimali <cassim.ali@outlook.fr>
2024-03-06 20:58:20 -0500

    Set up feature-connexion : Create LoginForm && Login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import forms
+
+def login_page(request):
+    form = forms.LoginForm()
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            pass
+    return render(request, 'authentication/login.html', context={'form': form })
